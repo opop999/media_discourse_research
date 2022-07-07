bb88945fffb815f8a674e87079117d228a526b86
Ondrej Pekacek <ondrej.pekacek@fsv.cuni.cz>
2022-07-07 18:21:13 +0200

    refactor: update shap_explainable_ml.py to account for sentence level workflow

--- docs/shap_explainable_ml.py
+++ docs/shap_explainable_ml.py
@@ -3,6 +3,7 @@
 
 import os
 import shap
+from torch import cuda
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
 from pyreadr import read_r
 
@@ -10,37 +11,48 @@ from pyreadr import read_r
 # https://huggingface.co/horychtom/czech_media_bias_classifier
 # Its tokenizer config, special tokens map and vocab are downloaded from:
 # https://huggingface.co/fav-kky/FERNET-C5
-model_path = "4.data_analysis/sentiment_analysis/data/model"
-path_to_input = "2.data_transformations/media_articles/data/regex_processed/chunks/"
+MODEL_PATH = "4.data_analysis/sentiment_analysis/data/model"
+PATH_TO_INPUT = "2.data_transformations/media_articles/data/udpipe_processed/chunks/"
 
 # Get list with all of the .rds files in the input directory.
-regex_files = [file for file in os.listdir(
-    path_to_input) if file.endswith(".rds")]
-regex_files.sort()
+udpipe_files = [file for file in os.listdir(
+    PATH_TO_INPUT) if file.endswith(".rds")]
+udpipe_files.sort()
 
 # Load one Czech text article from one dataset to serve as a demonstration example for SHAP.
-example_article = read_r(path_to_input + regex_files[0])[None].iloc[0, 1]
+udpipe_articles = read_r(PATH_TO_INPUT + udpipe_files[0])[None][["doc_id", "sentence_id", "token"]] \
+    .groupby(['doc_id', "sentence_id"], sort=False, as_index=False) \
+    .agg(
+    tokens=("token", " ".join)) \
+    .groupby(['doc_id'], sort=False, as_index=False) \
+    .agg(
+    text=("tokens", list)
+).iloc[0, 1][:10]  # get first 10 sentences, since SHAP calculation takes some time.
+
 
 # Testing with text (not token!) classification models, as SHAP does not seem to support them.
 tokenizer = AutoTokenizer.from_pretrained(
-    pretrained_model_name_or_path=model_path, model_max_length=512)
-model = AutoModelForSequenceClassification.from_pretrained(model_path)
-# Note that we have set top_k=1 for the pipeline so we can observe the model’s behavior for all classes, not just the top output.
-classifier = pipeline('text-classification', model=model, tokenizer=tokenizer,
-                      top_k=1)
+    pretrained_model_name_or_path=MODEL_PATH, model_max_length=512)
+model = AutoModelForSequenceClassification.from_pretrained(MODEL_PATH)
+# Note that we have set top_k=3 for the pipeline so we can observe the model’s behavior for all classes, not just the top output.
+classifier = pipeline("sentiment-analysis",
+                      model=model,
+                      tokenizer=tokenizer,
+                      device=0 if cuda.is_available() else -1,
+                      max_length=512,
+                      padding="longest",
+                      truncation=True,
+                      top_k=3)
+
 # Set up SHAP pipeline
 explainer = shap.Explainer(classifier)
 
-# Because the model only accepts a maximum of 512 tokens, we need to shorten the length of the text.
-# Since SHAP calculation takes some time, we will use only first 50 words.
-short_text = [tokenizer.decode(tokenizer.encode(
-    example_article, truncation=True, max_length=50, add_special_tokens=False))]
-
 # Model inference
-classifier(short_text)
+results = classifier(udpipe_articles)
+print(results)
 # Get values for SHAP
-shap_values = explainer(short_text)
+shap_values = explainer(udpipe_articles)
 
 # Visualize the results (works in Jupyter or other IPython environments)
-shap.plots.text(shap_values[:, :, ])
+shap.plots.text(shap_values[0, :, :])
 shap.plots.bar(shap_values[0, :, "LABEL_0"])
