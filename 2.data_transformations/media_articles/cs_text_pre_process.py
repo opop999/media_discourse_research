1fd3878a02045c8d39d6aa82ec3d8659f40d5965
Ondrej Pekacek <ondrej.pekacek@fsv.cuni.cz>
2022-07-09 20:40:52 +0200

    refactor: improve documentation and error handling

--- 2.data_transformations/media_articles/cs_text_pre_process.py
+++ 2.data_transformations/media_articles/cs_text_pre_process.py
@@ -1,21 +1,21 @@
-# Function to replace text in the column of Pandas dataframe using regex
-def pattern_preprocessing_cs(df, column, pattern):
-    """_summary_
+"""Function to replace text in the column of Pandas dataframe using regex pattern"""
+import pandas as pd
+
+
+def pattern_preprocessing_cs(input_df: pd.DataFrame, column: str, pattern: str) -> pd.DataFrame:
+    """
 
     Args:
-        df (_type_): _description_
-        column (_type_): _description_
-        pattern (_type_): _description_
+        input_df (pd.DataFrame): Pandas dataframe to be processed.
+        column (str): Column to be processed.
+        pattern (str): Pattern to be used.
 
     Returns:
-        _type_: _description_
+        pd.DataFrame: Processed dataframe.
     """
-    import pandas as pd
-    
-    df[column] = df[column].str.replace(pattern, " ", regex=True) \
-                .str.replace(r"\.{2,}", ".", regex=True) \
-                .str.strip() \
-                .str.replace(r"  +", " ", regex=True)
-    return df
 
-
+    input_df[column] = input_df[column].str.replace(pattern, " ", regex=True) \
+        .str.replace(r"\.{2,}", ".", regex=True) \
+        .str.strip() \
+        .str.replace(r"  +", " ", regex=True)
+    return input_df
