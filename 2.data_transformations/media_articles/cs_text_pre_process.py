4aa707c79eecff9a6969ca288fdd1d20256f0b02
Ondrej Pekacek <ondrej.pekacek@fsv.cuni.cz>
2022-07-10 14:04:16 +0200

    feat: only process pandas series, not entire dataframe

--- 2.data_transformations/media_articles/cs_text_pre_process.py
+++ 2.data_transformations/media_articles/cs_text_pre_process.py
@@ -1,21 +1,20 @@
 """Function to replace text in the column of Pandas dataframe using regex pattern"""
+
 import pandas as pd
 
 
-def pattern_preprocessing_cs(input_df: pd.DataFrame, column: str, pattern: str) -> pd.DataFrame:
+def pattern_preprocessing_cs(input_column: "pd.Series[str]", pattern: str) -> "pd.Series[str]":
     """
 
     Args:
-        input_df (pd.DataFrame): Pandas dataframe to be processed.
-        column (str): Column to be processed.
+        input_column (pd.DataFrame): Pandas series of strings to be preprocessed.
         pattern (str): Pattern to be used.
 
     Returns:
-        pd.DataFrame: Processed dataframe.
+        pd.Series[str]: Processed Pandas series.
     """
 
-    input_df[column] = input_df[column].str.replace(pattern, " ", regex=True) \
+    return input_column.str.replace(pattern, " ", regex=True) \
         .str.replace(r"\.{2,}", ".", regex=True) \
         .str.strip() \
         .str.replace(r"  +", " ", regex=True)
-    return input_df
