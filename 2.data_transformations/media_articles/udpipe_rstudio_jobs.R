48a6fe9f70ebf41924ba998e08f30974f20281ab
Ondrej Pekacek <ondrej.pekacek@fsv.cuni.cz>
2022-07-14 17:40:34 +0200

    refactor: simplify code, organize repository

--- 2.data_transformations/media_articles/udpipe_rstudio_jobs.R
+++ 2.data_transformations/media_articles/udpipe_rstudio_jobs.R
@@ -1,20 +1,20 @@
 source(file.path("udpipe_api_process.R"))
 
-for (o in seq_along(all_chunks_path_udpipe)) {
+for (regex_file in all_regex_chunks) {
 
-  one_chunk <- readRDS(all_chunks_path_udpipe[[o]])
+  one_chunk <- readRDS(regex_file)
 
   udpipe_df_chunk <- udpipe_process(article_id = one_chunk$article_id,
                                     article_text = one_chunk$text,
                                     log_path = file.path("docs/"))
 
-  saveRDS(udpipe_df_chunk, paste0(file.path("data", "udpipe_processed", "chunks", "udpipe_"), all_chunks_name_udpipe[[o]]))
+  saveRDS(udpipe_df_chunk, paste0(file.path("data", "udpipe_processed", "chunks", "udpipe_"), basename(regex_file)))
 
   rm(udpipe_df_chunk, one_chunk)
 
   gc()
 
-  cat("\nThe following chunk was processed by UDPIPE and saved locally:", all_chunks_name_udpipe[[o]], "\n")
+  cat("\nThe following chunk was processed by UDPIPE and saved locally:", basename(regex_file), "\n")
 
   # Pause ~10 mins between chunks
   Sys.sleep(abs(rnorm(1, 600, sd = 100)))
