d5726decf1e0afb8f2cb4de9c333524da92e4d34
Ondrej Pekacek <ondrej.pekacek@fsv.cuni.cz>
2022-11-27 15:45:38 +0000

    add documentation

--- 3.data_EDA/regex_filter_in_python.py
+++ 3.data_EDA/regex_filter_in_python.py
@@ -1,7 +1,13 @@
+# import required libraries
 import pandas as pd
+
+# load data from feather file
 df = pd.read_feather(
     "/home/andy/Media_discourse_research/1.data_sources/twitter/data/binded_tweets.feather")
+
+# filter tweets that contain keywords related to migration
 df_migration = df[df['text'].str.contains(
     "(?i)(běženec\w*)|(běženk\w*)|(imigrant\w*)|(migra\w*)|(imigra\w*)|(přistěhoval\w*)|(uprchl\w*)|(utečen\w*)|(azylant\w*)")]
-df_migration.shape
+
+# save filtered tweets to feather file
 df_migration.reset_index().to_feather("python_migration_filtered.feather")
